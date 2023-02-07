376a81ff8720e3d010640049b9482d56eb14d69f
Anas Bashandy <anasmbash@icloud.com>
2023-02-07 18:42:24 +0200

    Create a new ToDoDetailViewController
    Add a scene embedded in a UINavigationController in the storyboard
    Adjust the identity, title
    Add a plus barButtonItem to navigate to the new view

---
+++ ToDoList/ToDoDetail/ToDoDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ToDoDetailViewController.swift
+//  ToDoList
+//
+//  Created by Anas Bashandy on 07/02/2023.
+//
+
+import UIKit
+
+class ToDoDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // set the title
+        title = "New To-Do"
+        
+        // Adjust title size as this is a sub page
+        navigationItem.largeTitleDisplayMode = .never
+    }
+}
