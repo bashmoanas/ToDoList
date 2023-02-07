94906987705929616e7d50283b0afc23230e0bdd
Anas Bashandy <anasmbash@icloud.com>
2023-02-07 18:51:03 +0200

    Implement Swipe to delete

--- ToDoList/ToDosList/ToDoViewController.swift
+++ ToDoList/ToDosList/ToDoViewController.swift
@@ -65,4 +65,15 @@ final class ToDoViewController: UIViewController, UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            toDos.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
 }
