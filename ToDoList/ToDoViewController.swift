7ea2163f35de62ed4d3024d1f751c096193f448a
Anas Bashandy <anasmbash@icloud.com>
2023-02-07 15:25:21 +0200

    Implement Table View Data Source
    Create an outlet for the tableView from the Storyboard to the View Controller
    Implement numberOfRowsInSection, and cellForRowAt
    Set the table view data source to be the current view controller

--- ToDoList/ToDoViewController.swift
+++ ToDoList/ToDoViewController.swift
@@ -10,7 +10,11 @@ import UIKit
 /// Manages the to-dos list views hierarchy
 ///
 /// It's main view is a UITableView subclass that shall contain all the user-entered to-dos.
-final class ToDoViewController: UIViewController {
+final class ToDoViewController: UIViewController, UITableViewDataSource {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet private var tableView: UITableView!
     
     // MARK: - Properties
     
@@ -28,6 +32,34 @@ final class ToDoViewController: UIViewController {
         
         // Make the title big as this is the main view
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Set the table view data source
+        tableView.dataSource = self
+    }
+    
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        toDos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "toDoCellIdentifier"
+        
+        // Dequeue a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        // Get the to-do to display
+        let toDo = toDos[indexPath.row]
+        
+        // Update the cell content
+        var content = cell.defaultContentConfiguration()
+        content.text = toDo.title
+        cell.contentConfiguration = content
+        
+        // return the cell
+        return cell
     }
     
 }
